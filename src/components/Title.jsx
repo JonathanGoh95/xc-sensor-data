2386c112d1c373ddee669dc8b466d844926fde18
Jonathan Goh <jonathangohmh@gmail.com>
2025-11-16 16:31:35 +0800

    Updated Image Directory

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,9 @@
+import exceltec from '../../images/exceltec.png'
+
 export default function Title(){
     return(
     <div class="flex flex-col items-center justify-center font-bold italic text-5xl gap-2">
-        <img class="mt-4" src="../../images/exceltec.png"></img>
+        <img class="mt-4" src={exceltec}></img>
         <h1 class="mb-4">Domestic Water Tank (DWT) Sensor Data</h1>
     </div>
     )
