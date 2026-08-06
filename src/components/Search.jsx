e777820cc8d47316de951ff34c0bb99906058c2d
Jonathan Goh <jonathangohmh@gmail.com>
2026-08-07 00:57:29 +0800

    Updated conditional rendering in Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -22,7 +22,7 @@ export default function Search({handleSubmit,setQuery,queryID,setQueryID,sensorT
             <form className="flex flex-col items-center justify-center gap-4 mt-6 px-4 md:px-0" onSubmit={handleSubmit}>
                 <label className="font-bold italic text-xl md:text-2xl">Select Sensor Type: </label>
                 <select className="text-xl md:text-2xl border-2 rounded-lg p-1 px-2 hover:cursor-pointer text-center" name="sensorType" defaultValue={sensorType} value={sensorType} onChange={({target})=>setSensorType(target.value)} required>
-                    <option value="" disabled hidden>Select Sensor Type</option>
+                    <option value="" disabled hidden>Select Sensor</option>
                     <option value="pkt">Heltec Packet Sender</option>
                     <option value="dwt">DWT Sensor</option>
                     <option value="bin">Bin Sensor</option>
@@ -37,8 +37,8 @@ export default function Search({handleSubmit,setQuery,queryID,setQueryID,sensorT
                     <option value="iaq">IAQ Sensor</option>
                     <option value="touch">Toilet Panel (Touchscreen)</option>
                 </select>
-                {sensorType != "pkt" && <label className="font-bold italic text-xl md:text-2xl">Enter Location ID: </label>}
-                {sensorType != "pkt" && <input className="border-2 text-lg md:text-2xl rounded-lg w-auto text-center" value={queryID} type='text' placeholder='Location ID' onChange={({target})=>setQueryID((target.value))} maxLength={4} required></input>}
+                {(sensorType !== "pkt" && sensorType !== "") && <label className="font-bold italic text-xl md:text-2xl">Enter Location ID: </label>}
+                {(sensorType !== "pkt" && sensorType !== "") && <input className="border-2 text-lg md:text-2xl rounded-lg w-auto text-center" value={queryID} type='text' placeholder='Location ID' onChange={({target})=>setQueryID((target.value))} maxLength={4} required></input>}
                 <button className="border-2 text-lg md:text-2xl rounded-lg pt-2 pb-2 pl-5 pr-5 cursor-pointer italic w-auto hover:cursor-pointer" type="submit">Search</button>
             </form>
         )
